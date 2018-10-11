445ee3030d36a753eb91782f4b8e2f839acc7742
Bilal Khalid <bkbilalarain@gmail.com>
2018-10-11 11:19:19 -0400

    Implemented snake body list and trailing functionality

--- Snake.py
+++ Snake.py
@@ -1,16 +1,37 @@
 import pygame
 pygame.init()
 
+def trail(tempHeadX, tempHeadY):
+    tempX = 0
+    tempY = 0
+    for snakeSegment in snake:
+        if snakeSegment == snakeHead:
+            continue
+        else:
+            tempX = snakeSegment.x
+            tempY = snakeSegment.y
+            snakeSegment.x = tempHeadX
+            snakeSegment.y = tempHeadY
+            tempHeadX = tempX
+            tempHeadY = tempY
+
 gameSize = (700, 500)
 black = (0, 0, 0)
+snakeColour = (0, 255, 0)
+red = (255, 0, 0)
 
-snakeSegment = 50
+snakeSegmentWidth = 50
 speed = 50
-snakeX = 300
-snakeY = 300
+snakeHeadX = 300
+snakeHeadY = 300
 direction = "right"
 
-delay = 1000
+snakeHead = pygame.Rect(snakeHeadX, snakeHeadY, snakeSegmentWidth, snakeSegmentWidth)
+snakeBody = snakeHead.copy()
+snakeBody.x -= snakeSegmentWidth
+snake = [snakeHead, snakeBody]
+
+delay = 250
 move_event = pygame.USEREVENT + 1
 pygame.time.set_timer(move_event, delay)
 
@@ -24,32 +45,39 @@ while run:
         if event.type == pygame.QUIT:
             run = False
         if event.type == pygame.KEYDOWN:
+            tempX = snakeHead.x
+            tempY = snakeHead.y
             if event.key == pygame.K_RIGHT:
-                snakeX += speed
+                snakeHead.x += speed
                 direction = "right"
             elif event.key == pygame.K_LEFT:
-                snakeX -= speed
+                snakeHead.x -= speed
                 direction = "left"
             elif event.key == pygame.K_DOWN:
-                snakeY += speed
+                snakeHead.y += speed
                 direction = "down"
             elif event.key == pygame.K_UP:
-                snakeY -= speed
+                snakeHead.y -= speed
                 direction = "up"
+            trail(tempX,tempY)
             pygame.event.clear()
             pygame.time.set_timer(move_event, delay)
         elif event.type == move_event:
+            tempX = snakeHead.x
+            tempY = snakeHead.y
             if direction == "right":
-                snakeX += speed
+                snakeHead.x += speed
             elif direction == "left":
-                snakeX -= speed
+                snakeHead.x -= speed
             elif direction == "down":
-                snakeY += speed
+                snakeHead.y += speed
             elif direction == "up":
-                snakeY -= speed
+                snakeHead.y -= speed
+            trail(tempX,tempY)
 
     screen.fill(black)
-    pygame.draw.rect(screen, (0,255,0), (snakeX, snakeY, snakeSegment, snakeSegment))
+    for snakeSegment in snake:
+        pygame.draw.rect(screen, snakeColour, snakeSegment)
     pygame.display.flip()
 
 pygame.quit()
