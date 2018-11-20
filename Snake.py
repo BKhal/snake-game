09366d42aa1f0eba8eab4f056eae51eb6b7879c6
Bilal Khalid <bkbilalarain@gmail.com>
2018-11-20 01:05:37 -0500

    Updated snake look and movement functionality
    Snake now has black border surrounding individual segments.
    
    Snake movement via arrow keys restricted to perpendicular movement relative to current heading.

--- Snake.py
+++ Snake.py
@@ -1,6 +1,7 @@
 import pygame
 pygame.init()
 
+# Causes segments in snake body to follow movement of leading segment
 def trail(tempHeadX, tempHeadY):
     tempX = 0
     tempY = 0
@@ -15,22 +16,28 @@ def trail(tempHeadX, tempHeadY):
             tempHeadX = tempX
             tempHeadY = tempY
 
+# Game properties
 gameSize = (700, 500)
 black = (0, 0, 0)
-snakeColour = (0, 255, 0)
 red = (255, 0, 0)
+green = (0, 255, 0)
 
+# Snake default properties
+snakeColour = green
 snakeSegmentWidth = 50
 speed = 50
 snakeHeadX = 300
 snakeHeadY = 300
 direction = "right"
+outlineWidth = 2
 
+# Building snake
 snakeHead = pygame.Rect(snakeHeadX, snakeHeadY, snakeSegmentWidth, snakeSegmentWidth)
 snakeBody = snakeHead.copy()
 snakeBody.x -= snakeSegmentWidth
 snake = [snakeHead, snakeBody]
 
+# Timer event properties
 delay = 250
 move_event = pygame.USEREVENT + 1
 pygame.time.set_timer(move_event, delay)
@@ -40,31 +47,52 @@ run = True
 screen = pygame.display.set_mode(gameSize)
 pygame.display.set_caption("Snake")
 
+
+
+# Main game
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        
+        tempX = snakeHead.x
+        tempY = snakeHead.y
+
+        # Arrow key events
         if event.type == pygame.KEYDOWN:
-            tempX = snakeHead.x
-            tempY = snakeHead.y
-            if event.key == pygame.K_RIGHT:
+
+            # Right key
+            if event.key == pygame.K_RIGHT and (direction == "up" or direction == "down"):
                 snakeHead.x += speed
                 direction = "right"
-            elif event.key == pygame.K_LEFT:
+                trail(tempX,tempY)
+                pygame.time.set_timer(move_event, delay)
+            
+            # Left key
+            elif event.key == pygame.K_LEFT and (direction == "up" or direction == "down"):
                 snakeHead.x -= speed
                 direction = "left"
-            elif event.key == pygame.K_DOWN:
+                trail(tempX,tempY)
+                pygame.time.set_timer(move_event, delay)
+            
+            # Down key
+            elif event.key == pygame.K_DOWN and (direction == "right" or direction == "left"):
                 snakeHead.y += speed
                 direction = "down"
-            elif event.key == pygame.K_UP:
+                trail(tempX,tempY)
+                pygame.time.set_timer(move_event, delay)
+
+            # Up key
+            elif event.key == pygame.K_UP and (direction == "right" or direction == "left"):
                 snakeHead.y -= speed
                 direction = "up"
-            trail(tempX,tempY)
+                trail(tempX,tempY)
+                pygame.time.set_timer(move_event, delay)
+
             pygame.event.clear()
-            pygame.time.set_timer(move_event, delay)
+
+        # Timer based movement
         elif event.type == move_event:
-            tempX = snakeHead.x
-            tempY = snakeHead.y
             if direction == "right":
                 snakeHead.x += speed
             elif direction == "left":
@@ -76,8 +104,11 @@ while run:
             trail(tempX,tempY)
 
     screen.fill(black)
+
+    # Rebuilding snake after movement
     for snakeSegment in snake:
-        pygame.draw.rect(screen, snakeColour, snakeSegment)
+        pygame.draw.rect(screen, black, snakeSegment)
+        pygame.draw.rect(screen, snakeColour, (snakeSegment.x + outlineWidth, snakeSegment.y + outlineWidth, snakeSegmentWidth - outlineWidth * 2, snakeSegmentWidth - outlineWidth * 2))
     pygame.display.flip()
 
 pygame.quit()
